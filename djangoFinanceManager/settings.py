59b6d9fdd555a8d58500ccdd2556ac9f3bb37805
booond <maksbond03@gmail.com>
2023-04-20 21:14:42 +0300

    fix: removed secret key from code, added .gitignore and .env_example

--- djangoFinanceManager/settings.py
+++ djangoFinanceManager/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings_page.html - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r7s-sss!=&%aftwuwl)u@)ayhw9ydtltn)ci*n^57x!fc@_ss)'
+os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
